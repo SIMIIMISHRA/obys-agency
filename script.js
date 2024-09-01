7c5e304da014c7239e7d164c1c8fecea73c06e64
Simi Mishra <simimishra2006@gmail.com>
2024-09-01 16:54:37 +0530

    commit loccomotive

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+function  locomotiveAnimation (
+
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("main").style.transform ? "transform" : "fixed"
+});
+
+
+
+tl.from(".purple p", {scale: 0.3, rotation:45, autoAlpha: 0, ease: "power2"})
+  .from(".line-3", {scaleX: 0, transformOrigin: "left center", ease: "none"}, 0)
+  .to(".purple", {backgroundColor: "#28a92b"}, 0);
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.refresh();
+
+);
+
+
+
 function loadingAnimation(){
     var tl = gsap.timeline();
 tl.from(".line h1", {
@@ -35,7 +76,6 @@ tl.to("#loader", {
 tl.from("#page1", {
   delay: 0.2,
   y: 1600,
-  opacity: 0,
   duration: 0.6,
   ease: Power4,
 });
@@ -44,14 +84,18 @@ tl.to("#loader", {
   display: "none",
 });
 tl.from("#nav", {
-  opacity:0
-}
-)
+  opacity:0,
+});
 
 tl.from("#hero1 h1, #hero2 h1,  #hero3 h2, #hero4 h1", {
   y:-120,
-  stagger: 0.2
-})
+  stagger: 0.2,
+});
+
+tl.from("#hero1 ","#page2" ,{
+  opacity:0,
+},"-=1.2");//it should run this code first//
+
 }
 
 function  cursorAnimation(){
@@ -70,4 +114,5 @@ function  cursorAnimation(){
 }
 
 loadingAnimation();
-cursorAnimation()
+cursorAnimation();
+locomotiveAnimation ();
